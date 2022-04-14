fc8af4c2b9ea10355a1833a12ca6963177721a55
MauricioChacana <101777304+MauricioChacana@users.noreply.github.com>
2022-04-14 10:22:47 -0400

    Formularios JQUERY

--- js/script.js
+++ js/script.js
@@ -16,6 +16,15 @@
     }, false);
 })();
 
+function validarRegistro() {
+
+    var password = document.getElementById("validationPass").value;
+    var password2 = document.getElementById("validationPass2").value; 
+    if (password2 != password) {
+        document.getElementById("resValPass2").innerHTML = "Las contraseñas deben ser iguales";
+    }
+}
+
 $(document).ready(function() {
     $("#btnLogin").click(function() {
         var Usuario = $("#rutLogin").val();
@@ -23,11 +32,31 @@ $(document).ready(function() {
         var res;
         if (Usuario == "200822110" && Contraseña == "pass") {
             res = "Inicio de sesion valido"
+            $("#resultadoLoginBien").html(res); 
+            $("#resultadoLoginMal").html("");  
         } else {
 
             res = "Usuario/Contraseña incorrecto"
+            $("#resultadoLoginMal").html(res);
+            $("#resultadoLoginBien").html("");   
+        }    
+    });
+
+    $("#ValidarPass").click(function() {
+        var password = $("#validationPass").val();
+        var password2 = $("#validationPass2").val();
+        var res;
+        if ((password2 == password) && (password2 != "")) {
+            res = "Contraseña valida"
+            $("#valPass").html(res);
+            $("#valPass2").html("");
+        } else {
+            res = "Las contraseñas no son iguales"
+            $("#valPass2").html(res);
+            $("#valPass").html("");
         }
-        alert(res)
+        
+
     });
 
 });
