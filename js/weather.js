60752d6bf541d0a8535ae07450e2e9813c5dd28b
MauricioChacana <101777304+MauricioChacana@users.noreply.github.com>
2022-04-21 10:46:38 -0400

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -1,38 +1,40 @@
-/* geo localizacion*/
+
 window.addEventListener('load', () => {
     let lon
     let lat
-    let temperaturaValor = document.getElementById(`temperatura-valor`)
+    let temperaturaValor = document.getElementById(`temperaturaValor`)
     let temperaturaDescripcion = document.getElementById(`temperatura-descripcion`)
     let ubicacion = document.getElementById(`ubicacion`)
-    let iconoAnimado = document.getElementById(`icono-animado`)
-    let vientoVelocidad = document.getElementById(`viento-velocidad`)
+    let iconoAnimado = document.getElementById(`Icono`)
+    let vientoVelocidad = document.getElementById(`vientoVelocidad`)
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(posicion => {
+    //ubicacion por ciudad
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=-32.8337&lon=-70.5983&lang=es&units=metric&appid=527af813823205f3a60509919a23380c`
+    console.log(url)
+    fetch(url)
+        .then(response => { return response.json() })
+        .then(data => {
 
-            //ubicacion por ciudad
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=Quillota,CL&lang=es&units=metric&appid=527af813823205f3a60509919a23380c`
+            var ubi = data.name
+            var icon = data.weather[0].icon
+            if (ubi==null){
+                ubi="No disponible por el momen"
+            }
+            ubicacion.textContent = data.name
 
-            console.log(url)
-            fetch(url)
-                .then(response => { return response.json() })
-                .then(data => {
+            temperaturaValor.textContent =  Math.round(parseFloat(data.main.temp)) + " °C"
+            
+            vientoVelocidad.textContent = "Velocidad del viento: " + data.wind.speed + " m/s"
 
-                    let ubi = data.name
-                    ubicacion.textContent = `${ubi}`
-
-                    let tempe = toString(data.main.temp)
-                    temperaturaValor.textContent = tempe
-
-                    console.log(data)
+            const urlIcon = `http://openweathermap.org/img/wn/${icon}.png`
+            iconoAnimado.src=  urlIcon
+            
+            console.log(data)
 
 
-
-                })
-                .catch(error => {
-                    console.log(error)
-                })
         })
-    }
+        .catch(error => {
+            console.log(error)
+        })
+
 })
