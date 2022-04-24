24b8ef8cd9c7bd17d9bc89e8ed567f2f8073fa07
MauricioChacana <101777304+MauricioChacana@users.noreply.github.com>
2022-04-23 23:48:24 -0400

    Cheque de Codigo y Finalizacion de api

--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,15 @@
 let buscarDireccion = document.getElementById(`buscarDireccion`)
-let lon
-let lat
 
 
-fetch("./js/pais.json")
-    .then(response => { return response.json() })
-    .then(data => {
-        console.log(data)
-
-
-    })
-
-
-
+//Configuraciones del la api de Trueway Geocoding (Obtener cordenadas desde la dirreccion ingresada)
+const CredencialesDireccion = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'trueway-geocoding.p.rapidapi.com',
+        'X-RapidAPI-Key': 'e20b1e171bmsh58d117850934993p1a68aejsn650d8db4d5b1'
+    }
+};
+//Al momento de cargar la pagina carga la informacion climatica de Los Andes, ya que el colegio se ubica alli
 window.addEventListener('load', () => {
 
     let temperaturaValor = document.getElementById(`temperaturaValor`)
@@ -21,37 +18,25 @@ window.addEventListener('load', () => {
     let iconoAnimado = document.getElementById(`icono-animado`)
     let vientoVelocidad = document.getElementById(`vientoVelocidad`)
 
-    //ubicacion por ciudad
+    //ubicacion por ciudad especifica
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=-32.8337&lon=-70.5983&lang=es&units=metric&appid=527af813823205f3a60509919a23380c`
-    console.log(url)
     fetch(url)
         .then(response => { return response.json() })
         .then(data => {
-
             var ubi = data.name
-            var icon = data.weather[0].icon
             if (ubi == null) {
                 ubi = "No disponible por el momento"
             }
             ubicacion.textContent = data.name
-
             temperaturaValor.textContent = Math.round(parseFloat(data.main.temp)) + " °C"
-
             //console.log(data.weather[0].description)
             let desc = data.weather[0].description
             temperaturaDescripcion.textContent = desc.toUpperCase()
 
             vientoVelocidad.textContent = `${data.wind.speed} m/s`
 
-            const urlIcon = `http://openweathermap.org/img/wn/${icon}.png`
-            iconoAnimado.src = urlIcon
-
-
-
-
-
             /* Iconos Animados */
-
+            //Selecciona el icono en base a la opciones de clima de la api
             switch (data.weather[0].main) {
                 case 'Thunderstorm':
                     iconoAnimado.src = 'animated/thunder.svg'
@@ -85,15 +70,12 @@ window.addEventListener('load', () => {
                     iconoAnimado.src = 'animated/cloudy-day-1.svg'
                     console.log('por defecto');
             }
-
-
         })
         .catch(error => {
             console.log(error)
         })
-
 })
-
+//Actualiza el clima en base a lo ingresado por el usuario
 function actualizarClima() {
     let temperaturaValor = document.getElementById(`temperaturaValor`)
     let temperaturaDescripcion = document.getElementById(`temperaturaDescripcion`)
@@ -101,73 +83,85 @@ function actualizarClima() {
     let iconoAnimado = document.getElementById(`icono-animado`)
     let vientoVelocidad = document.getElementById(`vientoVelocidad`)
     let buscarDireccion = document.getElementById(`buscarDireccion`).value
-    let tex1 = buscarDireccion
-    tex1 = tex1.replace(/ /g, "%20")
-    tex1 = tex1.replace(/,/g, "%20")
-    var lat=  -33.015347
-    var lon=  -71.550026
-    url2 =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=527af813823205f3a60509919a23380c`
-    console.log(url2)
-    fetch(url2)
-        .then(response => { return response.json() })
-        .then(data2 => {
+    let direccion = buscarDireccion
 
-            var ubi = data2.name
-            var icon = data2.weather[0].icon
-            if (ubi == null) {
-                ubi = "No disponible por el momento"
-            }
-            ubicacion.textContent = data2.name
+    if (direccion != "") {
+        direccion = direccion.replace(/ /g, "%20") //Elimina los espacios en blanco y los remplaza por %20
+        direccion = direccion.replace(/,/g, "%2C") //Elimina las comas  y las remplaza por %2C
+        var lat
+        var lon
+        //La dirrecion se actualiza en base a lo ingresado
+        urlDireccion = `https://trueway-geocoding.p.rapidapi.com/Geocode?address=${direccion}&language=es&country=.cl`
 
-            temperaturaValor.textContent = Math.round(parseFloat(data2.main.temp)) + " °C"
+        fetch(urlDireccion, CredencialesDireccion)
+            .then(response => response.json())
+            .then(info => {
+                console.log(info)
+                lat = info.results[0].location.lat
+                lon = info.results[0].location.lng
+                //Se utiliza la latitud y longitud de la direccion ingresada para actualizar el clima
+                url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&units=metric&appid=527af813823205f3a60509919a23380c`
 
-            //console.log(data.weather[0].description)
-            let desc = data2.weather[0].description
-            temperaturaDescripcion.textContent = desc.toUpperCase()
+                console.log(url2)
 
-            vientoVelocidad.textContent = `${data2.wind.speed} m/s`
+                fetch(url2)
+                    .then(response => { return response.json() })
+                    .then(data2 => {
 
-            const urlIcon = `http://openweathermap.org/img/wn/${icon}.png`
-            console.log(data2.weather[0].main)
-            /* Iconos Animados */
-            switch (data2.weather[0].main) {
-                case 'Thunderstorm':
-                    iconoAnimado.src = 'animated/thunder.svg'
-                    console.log('TORMENTA');
-                    break;
-                case 'Drizzle':
-                    iconoAnimado.src = 'animated/rainy-2.svg'
-                    console.log('LLOVIZNA');
-                    break;
-                case 'Rain':
-                    iconoAnimado.src = 'animated/rainy-7.svg'
-                    console.log('LLUVIA');
-                    break;
-                case 'Snow':
-                    iconoAnimado.src = 'animated/snowy-6.svg'
-                    console.log('NIEVE');
-                    break;
-                case 'Clear':
-                    iconoAnimado.src = 'animated/day.svg'
-                    console.log('LIMPIO');
-                    break;
-                case 'Atmosphere':
-                    iconoAnimado.src = 'animated/weather.svg'
-                    console.log('ATMOSFERA');
-                    break;
-                case 'Clouds':
-                    iconoAnimado.src = 'animated/cloudy-day-1.svg'
-                    console.log('NUBES');
-                    break;
-                default:
-                    iconoAnimado.src = 'animated/cloudy-day-1.svg'
-                    console.log('por defecto');
-            }
+                        var ubi = data2.name
+                        if (ubi == null) {
+                            ubi = "No disponible por el momento"
+                        }
+                        ubicacion.textContent = data2.name
+                        console.log(data2)
+                        temperaturaValor.textContent = Math.round(parseFloat(data2.main.temp)) + " °C"
+                        let desc = data2.weather[0].description
+                        temperaturaDescripcion.textContent = desc.toUpperCase()
 
-
-        })
-        .catch(error => {
-            console.log(error)
-        })
+                        vientoVelocidad.textContent = `${data2.wind.speed} m/s`
+                        switch (data2.weather[0].main) {
+                            case 'Thunderstorm':
+                                iconoAnimado.src = 'animated/thunder.svg'
+                                console.log('TORMENTA');
+                                break;
+                            case 'Drizzle':
+                                iconoAnimado.src = 'animated/rainy-2.svg'
+                                console.log('LLOVIZNA');
+                                break;
+                            case 'Rain':
+                                iconoAnimado.src = 'animated/rainy-7.svg'
+                                console.log('LLUVIA');
+                                break;
+                            case 'Snow':
+                                iconoAnimado.src = 'animated/snowy-6.svg'
+                                console.log('NIEVE');
+                                break;
+                            case 'Clear':
+                                iconoAnimado.src = 'animated/day.svg'
+                                console.log('LIMPIO');
+                                break;
+                            case 'Atmosphere':
+                                iconoAnimado.src = 'animated/weather.svg'
+                                console.log('ATMOSFERA');
+                                break;
+                            case 'Clouds':
+                                iconoAnimado.src = 'animated/cloudy-day-1.svg'
+                                console.log('NUBES');
+                                break;
+                            default:
+                                iconoAnimado.src = 'animated/cloudy-day-1.svg'
+                                console.log('por defecto');
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error)
+                    })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    else {
+    }
 
 }
