19806188e2deddec709a6d180a48c3d23090c534
MauricioChacana <101777304+MauricioChacana@users.noreply.github.com>
2022-04-25 22:22:24 -0400

    Update Scroll Js
    Ahora en el nav se usa una funcion Js para llamar ir a la seccion

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@
     }, false);
 })();
 
+
 function validarRegistro() {
     var password = document.getElementById("validationPass").value;
     var password2 = document.getElementById("validationPass2").value;
@@ -24,6 +25,14 @@ function validarRegistro() {
         document.getElementById("resValPass2").innerHTML = "Las contraseñas deben ser iguales";
     }
 }
+
+function saltarA(id, tiempo) { //Funcion para ir a cualquier parte del sitio
+    var tiempo = tiempo || 10; //Tiempo que tarda en reflejar la accion en ms; por defecto son 10ms
+    console.log(tiempo)
+    $("html, body").animate({ scrollTop: $(id).offset().top }, tiempo); //Se mueve hasta el inicio del id
+
+  }
+
 
 $(document).ready(function() {
     $("#btnLogin").click(function() {
@@ -57,6 +66,6 @@ $(document).ready(function() {
         }
     });
 
-
+    
 });
 
